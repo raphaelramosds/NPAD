ce84f44aa85d822ad0d9a972e33fd25031eb7c2f
Raphael Ramos <raphaelramosds@gmail.com>
2024-07-10 15:52:12 -0300

    JSON files of Pascal Suite runnings

--- pascal-suite-tutorial-main/pascalanalyzer_job.sh
+++ pascal-suite-tutorial-main/pascalanalyzer_job.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#SBATCH --partition=intel-128
 #SBATCH --job-name=PaScal_job
 #SBATCH --output=PaScal_job%j.out
 #SBATCH --error=PaScal_job%j.err
@@ -8,4 +9,6 @@
 #SBATCH --time=0-0:10
 
 pascalanalyzer -t aut -c 1,2,4,8,16,32 -i 500,707,1000,1414,2000 -o LU.json ./LU
+pascalanalyzer -g -t aut -c 1,2,4,8,16,32 -i 500,707,1000,1414,2000 -o LU_g.json ./LU
+
 
