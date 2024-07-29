ff04ac9740668025c628c9ddd1fc5de34af321e9
Raphael Ramos <raphaelramos@geowellex.com>
2024-07-29 11:25:16 -0300

    Debugging ping pong

--- MPI/assignments/mpi_pp_clock.c
+++ MPI/assignments/mpi_pp_clock.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <time.h>
 
-const int LIMIT = 500;
+const int LIMIT = 5;
 
 int main(void) {
     int response = 0;
@@ -32,21 +32,25 @@ int main(void) {
         if (my_rank == 0) {
             start_t = clock();
 
+            // printf("[%d] Sending to process 1\n", response);
             MPI_Send(&response, 1, MPI_INT, 1, 0, comm);
+
             MPI_Recv(&response, 1, MPI_INT, 1, 1, comm, MPI_STATUS_IGNORE);
+            // printf("[%d] Received from process 1\n", response);
 
             end_t = clock();
             total_t = (double) (end_t - start_t) / CLOCKS_PER_SEC;
 
-            if (total_t > 0) {
-                printf("Elapsed (%d ping-pong): %fs\n", response, total_t);
-                MPI_Abort(comm, 1);
-            }
+            printf("Elapsed (%dth ping-pong): %fs\n", response, total_t);
 
         } else {
             MPI_Recv(&response, 1, MPI_INT, 0, 0, comm, MPI_STATUS_IGNORE);
+            // printf("[%d] Received from process 0\n", response);
+
+            // printf("[%d] Sending to process 0\n", response);
             MPI_Send(&response, 1, MPI_INT, 0, 1, comm);
         }
+
         response++;
     }
     
